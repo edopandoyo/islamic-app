53efb8bf563461960d519ff14e03768ff95c3c58
edo hadi pandoyo <edopandoyo@gmail.com>
2021-08-08 19:32:54 +0700

    perubahan data api ayat

--- src/components/DetailSurah.vue
+++ src/components/DetailSurah.vue
@@ -1,9 +1,17 @@
 <template>
-  <div v-for="item in ayat" :key="item.id" class="p-3 edo-rounded border mb-2">
+<h1>detail surah</h1>
+  <!-- <div v-for="item in ayat" :key="item.id" class="p-3 edo-rounded border mb-2">
     <p class="text-end arab">
       {{ item.text.arab }} - <span class=" ps-2 pe-2 m-0 rounded-pill border text-dark no-ayat">{{ item.number.inSurah }}</span>
     </p>
     <p class="text-start font-italic">Artinya: "{{ item.translation.id }}"</p>
+  </div> -->
+  <div v-for="item in ayat" :key="item.id"  class="p-3 edo-rounded border mb-2">
+    <p class="text-end arab">
+      {{item.aya_text}} - <span class=" ps-2 pe-2 m-0 rounded-pill border text-dark no-ayat">{{ item.aya_number }}</span>
+    </p>
+    <p class="text-start font-italic">Artinya: "{{ item.translation_aya_text }}"</p>
+    
   </div>
 </template>
 
@@ -18,9 +26,9 @@ export default {
   },
   created() {
     //   const api_url = "https://api.quran.sutanlab.id/surah/"{{surah}}
-    axios.get("https://api.quran.sutanlab.id/surah/"+this.$route.params.surah).then((response) => {
-      this.ayat = response.data.data.verses;
-      console.log(response);
+    axios.get("https://quran-api.masedo.online/api/"+this.$route.params.surah).then((response) => {
+      this.ayat = response.data["data"];
+      console.log(response.data["data"]);
     });
   },
 };
